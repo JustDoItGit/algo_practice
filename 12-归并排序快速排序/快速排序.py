94b8b45bedbdce161fe05f3275739e560c80b21b
cool <cool_kj@126.com>
2019-09-26 15:31:03 +0800

    快速排序

---
+++ 12-归并排序快速排序/快速排序.py
@@ -0,0 +1,39 @@
+import random
+
+
+def quick_sort(nums):
+    _quick_sort_between(nums, 0, len(nums) - 1)
+
+
+def _quick_sort_between(nums, low, high):
+    if low < high:
+        # k = random.randint(low, high)
+        # nums[low], nums[k] = nums[k], nums[low]
+
+        m = _partition(nums, low, high)
+        _quick_sort_between(nums, low, m - 1)
+        _quick_sort_between(nums, m + 1, high)
+
+
+def _partition(nums, low, high):
+    pivot, j = nums[low], low
+    for i in range(low + 1, high + 1):
+        if nums[i] <= pivot:
+            j += 1
+            nums[j], nums[i] = nums[i], nums[j]
+    nums[low], nums[j] = nums[j], nums[low]
+    return j
+
+
+test_ls = [3, 5, 38, 2, 1]
+quick_sort(test_ls)
+print(test_ls)
+test_ls = [3]
+quick_sort(test_ls)
+print(test_ls)
+test_ls = [3, 4]
+quick_sort(test_ls)
+print(test_ls)
+test_ls = [5, 2, 3, 1]
+quick_sort(test_ls)
+print(test_ls)
