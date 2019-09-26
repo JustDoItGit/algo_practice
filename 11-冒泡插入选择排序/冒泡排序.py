57977278466df00ed7e8b41a04b88b3887f49dd9
cool <cool_kj@126.com>
2019-09-26 23:09:57 +0800

    优化flag的位置

--- 11-冒泡插入选择排序/冒泡排序.py
+++ 11-冒泡插入选择排序/冒泡排序.py
@@ -2,13 +2,14 @@ def bubble(ls):
     n = len(ls)
     if n <= 1:
         return ls
-    flag = False
+
     for i in range(n):
+        flag = False
         for j in range(n - i - 1):
             if ls[j] > ls[j + 1]:
                 ls[j], ls[j + 1] = ls[j + 1], ls[j]
                 flag = True
-        if flag is not True:
+        if flag is False:
             return ls
     return ls
 
@@ -18,4 +19,6 @@ print(bubble(test_ls))
 test_ls = [3]
 print(bubble(test_ls))
 test_ls = [5, 2, 3, 1]
+print(bubble(test_ls))
+test_ls = [1, 2, 2, 4]
 print(bubble(test_ls))
