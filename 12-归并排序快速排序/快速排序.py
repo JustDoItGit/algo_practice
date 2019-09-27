c41d311ed9f4ba92443de3c9d22a515f8642dc57
cool <cool_kj@126.com>
2019-09-28 00:00:19 +0800

    默写完快速排序

--- 12-归并排序快速排序/快速排序.py
+++ 12-归并排序快速排序/快速排序.py
@@ -1,5 +1,6 @@
 import random
 
+
 # 空间复杂度O(1) 原地排序
 def quick_sort(nums):
     _quick_sort_between(nums, 0, len(nums) - 1)
@@ -16,13 +17,13 @@ def _quick_sort_between(nums, low, high):
 
 
 def _partition(nums, low, high):
-    pivot, j = nums[low], low
-    for i in range(low + 1, high + 1):
-        if nums[i] <= pivot:
-            j += 1
+    pivot, i = nums[high], low
+    for j in range(low, high):
+        if nums[j] < pivot:
             nums[j], nums[i] = nums[i], nums[j]
-    nums[low], nums[j] = nums[j], nums[low]
-    return j
+            i += 1
+    nums[high], nums[i] = nums[i], nums[high]
+    return i
 
 
 test_ls = [3, 5, 38, 2, 1]
