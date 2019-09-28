453de44f8365d67badc4f5303189e5f89147f26c
cool <cool_kj@126.com>
2019-09-28 22:27:16 +0800

    二分查找递归写法

---
+++ 15-二分查找/bsearch_recursion.py
@@ -0,0 +1,19 @@
+def bsearch(nums, target):
+    return bsearch_internally(nums, 0, len(nums) - 1, target)
+
+
+def bsearch_internally(nums, low, high, target):
+    if low > high:
+        return -1
+
+    mid = low + (high - low) // 2
+    if nums[mid] == target:
+        return mid
+    elif nums[mid] < target:
+        return bsearch_internally(nums, mid + 1, high, target)
+    else:
+        return bsearch_internally(nums, low, mid - 1, target)
+
+
+test_nums, tar = [1, 2, 3, 4, 5], 4
+print(bsearch(test_nums, tar))
